fa8d2bda5e496bcec46268ae4e8ca5b86c2e4edb
joshpson <jpearson846@gmail.com>
2018-10-12 20:20:20 -0400

    switched back to array

--- src/js/grid.js
+++ src/js/grid.js
@@ -2,31 +2,57 @@ import Tile from "./tile.js";
 
 export default class Grid {
   constructor(obj) {
-    this.tiles = { 1: {}, 2: {}, 3: {}, 4: {} };
+    this.tiles = [];
+    this.positions = {
+      1: {
+        1: false,
+        2: false,
+        3: false,
+        4: false
+      },
+      2: {
+        1: false,
+        2: false,
+        3: false,
+        4: false
+      },
+      3: {
+        1: false,
+        2: false,
+        3: false,
+        4: false
+      },
+      4: {
+        1: false,
+        2: false,
+        3: false,
+        4: false
+      }
+    };
+    this.createTile();
+    this.createTile();
   }
 
   transform(keyCode) {
-    Object.keys(this.tiles).forEach(row => {
-      Object.values(this.tiles[row]).forEach(tile => {
-        let rowClass = tile.className.charAt(5);
-        let colClass = tile.className.charAt(7);
-        switch (keyCode) {
-          case 37: //left
-            tile.updateClass(`tile-${rowClass}-1`);
-            break;
-          case 38: //up
-            tile.updateClass(`tile-1-${colClass}`);
-            break;
-          case 39: //right
-            tile.updateClass(`tile-${rowClass}-4`);
-            break;
-          case 40: //down
-            tile.updateClass(`tile-4-${colClass}`);
-            break;
-          default:
-            break;
-        }
-      });
+    this.tiles.forEach(tile => {
+      let row = tile.className.charAt(5);
+      let col = tile.className.charAt(7);
+      switch (keyCode) {
+        case 37: //left
+          tile.updateClass(`tile-${row}-1`);
+          break;
+        case 38: //up
+          tile.updateClass(`tile-1-${col}`);
+          break;
+        case 39: //right
+          tile.updateClass(`tile-${row}-4`);
+          break;
+        case 40: //down
+          tile.updateClass(`tile-4-${col}`);
+          break;
+        default:
+          break;
+      }
     });
   }
 
@@ -34,22 +60,22 @@ export default class Grid {
     let cell = Math.floor(Math.random() * 16 + 1);
     let row = Math.ceil(cell / 4);
     let col = (cell % 4) + 1;
-    if (this.tiles[row][col]) {
+    if (this.positions[row][col]) {
+      console.log("positions");
       this.createTile();
     } else {
       let tile = new Tile({ className: `tile-${row}-${col}` });
-      this.tiles[row][cell] = tile;
-      return tile;
+      this.positions[cell] = tile;
+      this.tiles.push(tile);
     }
   }
 
   returnContainer() {
     let div = document.createElement("div");
     div.className = "grid";
-    let tile1 = this.createTile();
-    let tile2 = this.createTile();
-    div.appendChild(tile1.createDiv());
-    div.appendChild(tile2.createDiv());
+    this.tiles.forEach(tile => {
+      div.appendChild(tile.createDiv());
+    });
     return div;
   }
 }
