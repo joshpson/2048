02268629b279568cb4ecfad98ee10e74d6101fa4
joshpson <jpearson846@gmail.com>
2018-10-13 19:22:53 -0400

    functioning mvp

--- src/js/grid.js
+++ src/js/grid.js
@@ -19,14 +19,85 @@ export default class Grid {
         this.createTile();
         break;
       case 38: //up
+        this.updateColUp(1);
+        this.updateColUp(2);
+        this.updateColUp(3);
+        this.updateColUp(4);
+        this.createTile();
         break;
       case 39: //right
+        this.updateRowRight(4);
+        this.updateRowRight(8);
+        this.updateRowRight(12);
+        this.updateRowRight(16);
+        this.createTile();
         break;
       case 40: //down
+        this.updateColDown(13);
+        this.updateColDown(14);
+        this.updateColDown(15);
+        this.updateColDown(16);
+        this.createTile();
         break;
       default:
         break;
     }
+  }
+
+  updateColUp(start) {
+    for (let i = start + 4; i <= start + 12; i += 4) {
+      for (let j = i; j > start; j -= 4) {
+        if (this.cells[j]) {
+          let tile = this.cells[j];
+          if (!this.cells[j - 4]) {
+            tile.updateCell(j - 4);
+            this.cells[j - 4] = tile;
+            delete this.cells[j];
+          } else if (
+            this.cells[j].value === this.cells[j - 4].value &&
+            !tile.merged
+          ) {
+            this.cells[j - 4].doubleValue();
+            this.cells[j - 4].merged = true;
+            tile.remove();
+            delete this.cells[j];
+          }
+        }
+      }
+    }
+    Object.values(this.cells).forEach(tile => {
+      if (tile.merged) {
+        tile.merged = false;
+      }
+    });
+  }
+
+  updateColDown(start) {
+    for (let i = start - 4; i >= start - 12; i -= 4) {
+      for (let j = i; j < start; j += 4) {
+        if (this.cells[j]) {
+          let tile = this.cells[j];
+          if (!this.cells[j + 4]) {
+            tile.updateCell(j + 4);
+            this.cells[j + 4] = tile;
+            delete this.cells[j];
+          } else if (
+            this.cells[j].value === this.cells[j + 4].value &&
+            !tile.merged
+          ) {
+            this.cells[j + 4].doubleValue();
+            this.cells[j + 4].merged = true;
+            tile.remove();
+            delete this.cells[j];
+          }
+        }
+      }
+    }
+    Object.values(this.cells).forEach(tile => {
+      if (tile.merged) {
+        tile.merged = false;
+      }
+    });
   }
 
   updateRowLeft(start) {
@@ -51,17 +122,45 @@ export default class Grid {
       }
     }
     Object.values(this.cells).forEach(tile => {
-      tile.merged = false;
+      if (tile.merged) {
+        tile.merged = false;
+      }
+    });
+  }
+
+  updateRowRight(start) {
+    for (let i = start - 1; i >= start - 3; i -= 1) {
+      for (let j = i; j < start; j += 1) {
+        if (this.cells[j]) {
+          let tile = this.cells[j];
+          if (!this.cells[j + 1]) {
+            tile.updateCell(j + 1);
+            this.cells[j + 1] = tile;
+            delete this.cells[j];
+          } else if (
+            this.cells[j].value === this.cells[j + 1].value &&
+            !tile.merged
+          ) {
+            this.cells[j + 1].doubleValue();
+            this.cells[j + 1].merged = true;
+            tile.remove();
+            delete this.cells[j];
+          }
+        }
+      }
+    }
+    Object.values(this.cells).forEach(tile => {
+      if (tile.merged) {
+        tile.merged = false;
+      }
     });
   }
 
   createTile() {
     let cell = Math.floor(Math.random() * 16 + 1);
     if (this.cells[cell]) {
-      console.log("dup");
       this.createTile();
     } else {
-      console.log("create");
       let tile = new Tile({ className: `cell-${cell}` });
       this.cells[cell] = tile;
       this.grab().appendChild(tile.createDiv());
